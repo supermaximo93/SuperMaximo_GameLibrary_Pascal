20224a64a63c4d531d7587cada8fd9d07e6f59ee
supermaximo93 <supermaximo93@gmail.com>
2011-08-26 22:35:09 +0100

    Added Sound

---
+++ smgl_sound.pas
@@ -0,0 +1,24 @@
+unit SMGL_Sound;
+
+interface
+
+uses SMGL_SMSDL;
+
+type
+  Sound = pointer;
+
+function soundNew(newName, fileName : pChar) : Sound; cdecl; external LIBNAME;
+procedure soundDelete(sound_ : Sound); cdecl; external LIBNAME;
+function soundName(sound_ : Sound) : pChar; cdecl; external LIBNAME;
+procedure soundSetVolume(sound_ : Sound; percentage : integer; relative : boolean); cdecl; external LIBNAME;
+function soundVolume(sound_ : Sound) : integer; cdecl; external LIBNAME;
+function soundPlay(sound_ : Sound; newVolume, channel : integer) : integer; cdecl; external LIBNAME;
+procedure soundStop(sound_ : Sound); cdecl; external LIBNAME;
+procedure soundSetSoundPosition(sound_ : Sound; angle, distance : integer); cdecl; external LIBNAME;
+
+procedure allocateSoundChannels(channels : cardinal); cdecl; external LIBNAME;
+
+implementation
+
+end.
+
